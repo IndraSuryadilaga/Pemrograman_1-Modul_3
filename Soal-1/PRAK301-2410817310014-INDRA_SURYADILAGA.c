d646f7aaf7c785ffae724d7bdff4ff8f8647374c
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:39:03 +0800

    Update PRAK301-2410817310014-INDRA_SURYADILAGA.c

--- Soal-1/PRAK301-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-1/PRAK301-2410817310014-INDRA_SURYADILAGA.c
@@ -2,26 +2,24 @@
 
 int main() {
     int bil_1, bil_2, bil_3;
-    int urutan_1, urutan_2, urutan_3;
-
     scanf("%d %d %d", &bil_1, &bil_2, &bil_3);
 
-    urutan_1 = bil_1;
-    urutan_2 = bil_2;
-    urutan_3 = bil_3;
+    int urutan_1 = bil_1;
+    int urutan_2 = bil_2;
+    int urutan_3 = bil_3;
 
-    if (bil_1 <= bil_2 && bil_1 <= bil_3) {
+    if (bil_1 >= bil_2 && bil_1 >= bil_3) {
         urutan_1 = bil_1;
-        if (bil_2 <= bil_3) {
+        if (bil_2 >= bil_3) {
             urutan_2 = bil_2;
             urutan_3 = bil_3;
         } else {
             urutan_2 = bil_3;
             urutan_3 = bil_2;
         }
-    } else if (bil_2 <= bil_1 && bil_2 <= bil_3) {
+    } else if (bil_2 >= bil_1 && bil_2 >= bil_3) {
         urutan_1 = bil_2;
-        if (bil_1 <= bil_3) {
+        if (bil_1 >= bil_3) {
             urutan_2 = bil_1;
             urutan_3 = bil_3;
         } else {
@@ -30,15 +28,15 @@ int main() {
         }
     } else {
         urutan_1 = bil_3;
-        if (bil_1 <= bil_2) {
+        if (bil_1 >= bil_2) {
             urutan_2 = bil_1;
             urutan_3 = bil_2;
         } else {
             urutan_2 = bil_2;
             urutan_3 = bil_1;
         }
-}
+    }
 
-printf("%d %d %d\n", urutan_1, urutan_2, urutan_3);
-return 0;
+    printf("%d %d %d\n", urutan_1, urutan_2, urutan_3);
+    return 0;
 }
