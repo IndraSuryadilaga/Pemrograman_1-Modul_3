4343c24a852e2325d327f59274fd753231d85d96
Indra Suryadilaga <indrasryd@gmail.com>
2024-12-29 17:41:21 +0800

    Update PRAK305-2410817310014-INDRA_SURYADILAGA.c

--- Soal-5/PRAK305-2410817310014-INDRA_SURYADILAGA.c
+++ Soal-5/PRAK305-2410817310014-INDRA_SURYADILAGA.c
@@ -2,21 +2,18 @@
 
 int main() {
     int jumlah_detik;
-    int jam, menit, detik, hari;
-
     scanf("%d", &jumlah_detik);
 
-    jam = jumlah_detik / 3600;
-    menit = (jumlah_detik % 3600) / 60;
-    detik = jumlah_detik % 60;
+    int jam = jumlah_detik / 3600;
+    int menit = (jumlah_detik % 3600) / 60;
+    int detik = jumlah_detik % 60;
 
     if (jam >= 24) {
-        hari = jam / 24;
-        jam = jam % 24;
+        int hari = jam / 24;
+        int jam = jam % 24;
         printf("%d hari %02d:%02d:%02d\n", hari, jam, menit, detik);
     } else {
         printf("%02d:%02d:%02d\n", jam, menit, detik);
     }
-
     return 0;
 }
